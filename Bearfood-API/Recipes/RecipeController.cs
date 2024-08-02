37c9f0ef86192288cfaab2ca59162d6ad7db6b29
rolandhobaj <rolandhobaj@gmail.com>
2024-08-02 10:29:31 +0200

    Add basic controller

---
+++ Bearfood-API/Recipes/RecipeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bearfood_API.Recipes;
+
+[ApiController]
+[Route("api/[controller]")]
+public class RecipeController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        var recipe = new { Id = 1, Name = "Leves" };
+        return Ok(recipe);
+    }
+}
